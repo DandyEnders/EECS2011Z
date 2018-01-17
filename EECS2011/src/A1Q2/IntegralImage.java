3278b0014fdea52247c955d1abe30952dab62dec
howden2 <howden2@gmail.com>
2018-01-17 15:38:04 -0500

    Fixed A1Q2

--- EECS2011/src/A1Q2/IntegralImage.java
+++ EECS2011/src/A1Q2/IntegralImage.java
@@ -10,8 +10,9 @@ package A1Q2;
 public class IntegralImage {
 
 	private final int[][] integralImage;
-	private final int imageHeight; // height of image (first index)
-	private final int imageWidth; // width of image (second index)
+	private final int[][] rectangularTotal;
+	private final int imageHeight; 			// height of image (first index)
+	private final int imageWidth; 			// width of image (second index)
 
 	/**
 	 * Constructs an integral image from the given input image.
@@ -39,11 +40,30 @@ public class IntegralImage {
 			}
 
 		}
-		
+
 		// initiallize image / Height / Width
 		this.integralImage = image;
+
 		this.imageHeight = integralImage.length;
 		this.imageWidth = integralImage[0].length;
+
+		// create a new 2D array containing rectangular sums
+
+		rectangularTotal = new int[imageHeight][imageWidth];
+		for (int i = 0; i < rectangularTotal.length; i++) {
+			for (int j = 0; j < rectangularTotal[0].length; j++) {
+				int rectangularPartialSum = 0;
+
+				for (int a = 0; a <= i; a++) {
+					for (int b = 0; b <= j; b++) {
+						rectangularPartialSum += integralImage[a][b]; // add all rectangular sums
+					}
+				}
+
+				rectangularTotal[i][j] = rectangularPartialSum; // sub into the
+
+			}
+		}
 
 	}
 
@@ -71,37 +91,40 @@ public class IntegralImage {
 	 */
 	public double meanSubImage(int top, int bottom, int left, int right)
 			throws BoundaryViolationException, NullSubImageException {
-		
-		if(
-				top < 0 						|| 	// if top is smaller then array height 0
-				bottom > integralImage.length 	|| 	// if bottom is bigger then array height
-				left < 0 						||	// if left is smaller then array width 0
-				right > integralImage[0].length)	// if right is bigger then array width
+
+		if (top < 0 || 								// if top is smaller then array height 0
+				bottom > integralImage.length ||	// if bottom is bigger then array height
+				left < 0 || 						// if left is smaller then array width 0
+				right > integralImage[0].length) 	// if right is bigger then array width
 		{
-			throw new BoundaryViolationException();	// throw exception
+			throw new BoundaryViolationException(); // throw exception
 		}
-		
-		if (
-				top > bottom || //-----------------	//if top is bigger then bottom
-				left > right //--------------------	//if left is bigger than right
-		)
-		{
-			throw new NullSubImageException();		//throw exception
+
+		if (top > bottom || // ---------------------//if top is bigger then bottom
+				left > right // --------------------//if left is bigger than right
+		) {
+			throw new NullSubImageException(); 		// throw exception
 		}
-		
+
 		double totalImageSum = 0;
+		int topRectangleSum = 0;
+		int leftRectangleSum = 0;
+		int rightBottomTotalSum = 0;
+		int overlapSum = 0;
 		
-		for(int a = top ; a <= bottom ; a++) {
-			for(int b = left ; b <= right ; b++ ) {
-				totalImageSum += integralImage[a][b];
-			}
-		}
+		if(top > 0){topRectangleSum = rectangularTotal[top-1][right];}
+		if(left > 0) {leftRectangleSum = rectangularTotal[bottom][left-1];}
+		rightBottomTotalSum = rectangularTotal[bottom][right];
 		
+		if(top > 0 && left > 0){overlapSum = rectangularTotal[top-1][left-1];}
+		
+		totalImageSum = rightBottomTotalSum - topRectangleSum - leftRectangleSum + overlapSum;
+
 		double n = (bottom - top) + (right - left) + 2;
-		
-		double mean = (double)(totalImageSum / n);
-		
+
+		double mean = (double) (totalImageSum / n);
+
 		return mean;
 	}
-	
+
 }
